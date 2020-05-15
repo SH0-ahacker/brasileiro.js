a3558659308c8efb24249aeb317350305c1b58f2
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2020-05-14 21:30:41 -0300

    Create .js

---
+++ scripts/console/.js
@@ -0,0 +1,18 @@
+function consoleAviso(message) {
+	console.warn(message)
+}
+consoleAvise = consoleAviso
+
+function consoleInfo(message) {
+	console.info(message)
+}
+consoleInforme = consoleInfo
+
+function consoleErro(message) {
+	console.error(message)
+}
+consoleErrinho = consoleErro
+
+function consoleLog(message) {
+	console.log(message)
+}
